e3a83f8841ba93434f8434001d547a4b5d74ddba
Garry Marsland <garry@basefour.co.uk>
2017-10-24 20:51:43 +0100

    Add generic configuration class Upgrade Example.Common to netstandard2.0

--- Example.Common/Configuration.cs
+++ Example.Common/Configuration.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 namespace Example.Common
 {
     public class Configuration
     {
+        public Configuration()
+        {
+            var configurationRoot = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         public static string AccessKey = "";
         public static string SecretKey = "";
     }
